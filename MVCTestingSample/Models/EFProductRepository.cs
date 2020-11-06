904eefaab515e4a0f16d7a1c80d3d3db017b5e0b
Dillon Stricklett <dillonstricklett@live.com>
2020-11-05 22:28:24 -0800

    Issue#4 create add select controller (#5)
    * Created interface
    
    * Created product controller
    
    * Installed NuGet package
    
    * Created and updated database
    
    * Added a product displaye page
    
    * Implemented ProductRepository

--- MVCTestingSample/Models/EFProductRepository.cs
+++ MVCTestingSample/Models/EFProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using MVCTestingSample.Models.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTestingSample.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,31 +9,61 @@ namespace MVCTestingSample.Models
 {
     public class EFProductRepository : IProductRepository
     {
+        private readonly ProductDbContext _context;
 
+        public EFProductRepository(ProductDbContext context)
+        {
+            _context = context;
+        }
 
+        /// <summary>
+        /// Adds a Product to the data store
+        /// </summary>
+        /// <param name="p">The Product</param>
+        /// <returns></returns>
         public Task AddProductAsync(Product p)
         {
-            throw new NotImplementedException();
+            _context.Add(p);
+            return _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Deletes a Product from the data store
+        /// </summary>
+        /// <param name="p">The Product</param>
+        /// <returns></returns>
         public Task DeleteProductAsync(Product p)
         {
-            throw new NotImplementedException();
+            _context.Remove(p);
+            return _context.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<Product>> GetAllProductsAsync()
+        /// <summary>
+        /// Returns all Products from the data store
+        /// </summary>
+        /// <returns>List of Products</returns>
+        public async Task<List<Product>> GetAllProductsAsync()
         {
-            throw new NotImplementedException();
+            return await _context.Products.OrderBy(p => p.Name).ToListAsync();
         }
 
-        public Task<Product> GetProductByIdAsync(int id)
+        /// <summary>
+        /// Returns the Product by the id,
+        /// or null if no product matches
+        /// </summary>
+        /// <param name="id">Product id</param>
+        /// <returns>Product</returns>
+        public async Task<Product> GetProductByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Products
+                           .Where(p => p.ProductId == id)
+                           .SingleOrDefaultAsync();
         }
 
         public Task UpdateProductAsync(Product p)
         {
-            throw new NotImplementedException();
+            _context.Entry(p).State = EntityState.Modified;
+            return _context.SaveChangesAsync();
         }
     }
 }
